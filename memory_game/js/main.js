717a9dc8b4fb764d7f43e411102a2b246db4587e
Hannah Brantley <hannahbrantley@Hannahs-MacBook-Pro.local>
2020-04-17 13:00:07 -0600

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,19 +1,43 @@
 console.log("Up and running!");
 
-let cards = ["queen", "queen", "king", "king"];
+let cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
+}
+];
+
 let cardsInPlay = [];
 
 function checkForMatch() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-		console.log("You found a match!");
+		alert("You found a match!");
 	} else { 
-		console.log("Sorry, try again.");
+		alert("Sorry, try again.");
 		}
 	}
 
 function flipCard(cardID) {
-	console.log("User flipped " + cards[cardID]);
-	cardsInPlay.push(cards[cardID]);
+	console.log("User flipped " + cards[cardID].rank);
+	cardsInPlay.push(cards[cardID].rank);
+	console.log(cards[cardID].cardImage);
+	console.log(cards[cardID].suit);
 	if (cardsInPlay.length === 2) {
 	checkForMatch();
 	}
@@ -21,6 +45,8 @@ function flipCard(cardID) {
 
 flipCard(0);
 flipCard(2);
+
+
 
 
 
